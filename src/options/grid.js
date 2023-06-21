c63c150fc5922b220857dab20aef95beb14b527a
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-06-21 22:55:39 +0300

    Исправляет ошибки линтера

--- src/options/grid.js
+++ src/options/grid.js
@@ -1,12 +1,13 @@
-import { Cell } from "./cell.js";
+/* eslint-disable no-param-reassign */
+import Cell from "./cell";
 
 const GRID_SIZE = 4;
 const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
 
-export class Grid {
+export default class Grid {
   constructor(gridElement) {
     this.cells = [];
-    for (let i = 0; i < CELLS_COUNT; i++) {
+    for (let i = 0; i < CELLS_COUNT; i=+ 1) {
       this.cells.push(
         new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
       );
